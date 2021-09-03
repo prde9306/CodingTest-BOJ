4a8e13d6c59a411237c3c3e881099f0f06120e0b
prde9306 <prde9306@gmail.com>
2021-09-03 19:16:48 +0900

    210903 codingTest review

--- BOJ/Backtracking/BOJ_1182.java
+++ BOJ/Backtracking/BOJ_1182.java
@@ -11,6 +11,7 @@ public class BOJ_1182 {
         N= sc.nextInt();
         S= sc.nextInt();
         arr = new int[N];
+
         for (int i = 0; i <N ; i++) {
             arr[i] =  sc.nextInt();
         }
@@ -23,8 +24,6 @@ public class BOJ_1182 {
         }else {
             System.out.println(count);
         }
-
-
     }
 
     private static void dfs(int v , int su){
@@ -37,6 +36,51 @@ public class BOJ_1182 {
         // 부분수열, 지금 위치의 원소를 선택하거, 선택하지 않거나
         dfs(v+1, su+arr[v]); // 지금 위치의 원소를 선택
         dfs(v+1, su); // 선택하지 않음.
+    }
+
+    //sol2
+
+    /**
+      내풀이 왜 if에서 두번 확인해줘야 하는지, 공집합 고려해야하는지!!
+     */
+    class Main{
+
+        static int N, M;
+        static int []arr;
+        static int answer=0;
+
+        public void DFS(int Level, int sum){
+
+            if(Level==N){
+                if(sum==M){
+                    answer++;
+                }
+            }
+            else{
+                DFS(Level+1, sum+arr[Level]);
+                DFS(Level+1, sum);
+            }
+
+        }
+
+
+        public static void main(String[] args) throws IOException{
+
+            Main T = new Main();
+            Scanner sc = new Scanner(System.in);
+
+            N = sc.nextInt();
+            M = sc.nextInt();
+
+            arr = new int[N];
+            for(int i=0; i<N; i++){
+                arr[i]=sc.nextInt();
+            }
+            T.DFS(0,0);
+            if(M==0){ answer--;
+                System.out.print(answer);}
+            else System.out.print(answer);
+        }
     }
 }
 
